6d37bc0d37ba4f0518099009bac7340cb5808f4e
Wolferine189 <92498047+Wolferine189@users.noreply.github.com>
2023-02-13 19:54:08 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function mainu(){
+    x=document.getElementById("ume");
+    y=document.getElementById("icon");
+    
+if ( x.style.display=="none") {
+    x.style.display="block";
+}
+
+else{
+    x.style.display="none";  
+}
+}
+
+
+
+
